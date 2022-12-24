7c6a9161cfa258b516828625fe79f84c74f10962
Jibao Mansaray <jibaomansaray@gmail.com>
2022-12-24 09:39:45 +0000

    App Helper: add authenticated struct
    This struct contains the authenticated user entity from
    the auth middleware.

---
+++ backend/src/app/app_helpers/authenticated_user.rs
@@ -0,0 +1,41 @@
+use std::future::{ready, Ready};
+
+use actix_web::{FromRequest, HttpMessage};
+
+use crate::user::user_entities::UserEntity;
+
+use super::not_authenticated_error::NotAuthenticatedError;
+
+#[derive(Debug)]
+pub struct AuthenticatedUser(pub UserEntity);
+impl FromRequest for AuthenticatedUser {
+    type Error = NotAuthenticatedError;
+    type Future = Ready<Result<Self, Self::Error>>;
+
+    fn from_request(
+        req: &actix_web::HttpRequest,
+        _payload: &mut actix_web::dev::Payload,
+    ) -> Self::Future {
+        let extensions = req.extensions();
+        let result = match extensions.get::<UserEntity>() {
+            Some(auth_user) => Ok(AuthenticatedUser(auth_user.clone())),
+            None => Err(NotAuthenticatedError::default()),
+        };
+
+        ready(result)
+    }
+}
+
+impl std::ops::Deref for AuthenticatedUser {
+    type Target = UserEntity;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl AuthenticatedUser {
+    pub fn entity(&self) -> UserEntity {
+        self.0.clone()
+    }
+}
