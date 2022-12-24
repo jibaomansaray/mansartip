ab1a75726bff5a013511788a2a16a5bb2030510e
Jibao Mansaray <jibaomansaray@gmail.com>
2022-12-24 14:53:08 +0000

    Country DTOs: Add `new` static method
    This makes it easy for controller to create a new
    instance.

--- backend/src/country/country_dtos/countries_api_response_dto.rs
+++ backend/src/country/country_dtos/countries_api_response_dto.rs
@@ -1,11 +1,28 @@
+use crate::country::country_entities::CountryEntity;
+
 use super::country_entity_api_dto::CountryEntityApiDto;
 use serde::{Deserialize, Serialize};
 
-
 #[derive(Debug, Serialize, Deserialize)]
 pub(crate) struct CountriesApiResponseDto {
-  pub success: bool,
-  pub countries: Vec<CountryEntityApiDto>,
-  pub message: Option<String>,
-  pub code: Option<String>,
+    pub success: bool,
+    pub countries: Vec<CountryEntityApiDto>,
+    pub message: Option<String>,
+    pub code: Option<String>,
+}
+
+impl CountriesApiResponseDto {
+    pub fn new(country_entities: Vec<CountryEntity>) -> Self {
+        let mut countries = Vec::new();
+
+        for a_country in country_entities {
+            countries.push(CountryEntityApiDto::from(a_country));
+        }
+        Self {
+            success: true,
+            countries,
+            message: None,
+            code: None,
+        }
+    }
 }
