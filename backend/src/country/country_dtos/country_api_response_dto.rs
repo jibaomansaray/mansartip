d354eab50dc30ab37ac0242680e85e5320caab25
Jibao Mansaray <jibaomansaray@gmail.com>
2022-12-24 14:17:56 +0000

    Country DTOs: Add api response for a country

---
+++ backend/src/country/country_dtos/country_api_response_dto.rs
@@ -0,0 +1,11 @@
+use super::country_entity_api_dto::CountryEntityApiDto;
+use serde::{Deserialize, Serialize};
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub(crate) struct CountryApiResponseDto {
+  pub success: bool,
+  pub countries: Option<CountryEntityApiDto>,
+  pub message: Option<String>,
+  pub code: Option<String>,
+}
