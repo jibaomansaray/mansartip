ec582f4f00a3900e3420f2fad109bf2d43eef5d0
Jibao Mansaray <jibaomansaray@gmail.com>
2022-12-24 14:54:23 +0000

    Country DTOs: Add two new methods
    - `new` to create a new instance
    - `new_not_found` for creating a new instance when a result
    is not found.

--- backend/src/country/country_dtos/country_api_response_dto.rs
+++ backend/src/country/country_dtos/country_api_response_dto.rs
@@ -1,11 +1,32 @@
+use crate::country::country_entities::CountryEntity;
+
 use super::country_entity_api_dto::CountryEntityApiDto;
 use serde::{Deserialize, Serialize};
 
-
 #[derive(Debug, Serialize, Deserialize)]
 pub(crate) struct CountryApiResponseDto {
-  pub success: bool,
-  pub country: Option<CountryEntityApiDto>,
-  pub message: Option<String>,
-  pub code: Option<String>,
+    pub success: bool,
+    pub country: Option<CountryEntityApiDto>,
+    pub message: Option<String>,
+    pub code: Option<String>,
+}
+
+impl CountryApiResponseDto {
+    pub fn new_not_found() -> Self {
+        Self {
+            success: false,
+            country: None,
+            message: Some("record not found".to_owned()),
+            code: Some("record_not_found".to_owned()),
+        }
+    }
+
+    pub fn new(country: CountryEntity) -> Self {
+        Self {
+            success: true,
+            code: None,
+            message: None,
+            country: Some(CountryEntityApiDto::from(country)),
+        }
+    }
 }
