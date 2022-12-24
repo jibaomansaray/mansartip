d242efaf787a64fe5ef3ec19d5d35ab6a1fbed72
Jibao Mansaray <jibaomansaray@gmail.com>
2022-12-24 14:58:22 +0000

    User routes: Register `user information` route

--- backend/src/user/user_routes.rs
+++ backend/src/user/user_routes.rs
@@ -1,10 +1,11 @@
-use actix_web::{Scope };
+use actix_web::Scope;
 
+use crate::user::user_controllers;
 
 pub fn register_api_routes(scope: Scope) -> Scope {
-  scope
+    scope.service(user_controllers::user_my_information_controller::handler)
 }
 
 pub fn register_public_routes(scope: Scope) -> Scope {
-  scope
+    scope
 }
