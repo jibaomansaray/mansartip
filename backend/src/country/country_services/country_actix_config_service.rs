9cc9159ef4688f87045c8c6422e58eb0e8aea1e0
Jibao Mansaray <jibaomansaray@gmail.com>
2022-12-24 14:01:09 +0000

    Country Services: Fromat code

--- backend/src/country/country_services/country_actix_config_service.rs
+++ backend/src/country/country_services/country_actix_config_service.rs
@@ -2,12 +2,11 @@ use actix_web::web;
 
 use crate::app::app_state::AppState;
 
-use super::{country_repo_service::CountryRepoService, country_service::CountryService};
+use super::{country_service::CountryService, country_repo_service::CountryRepoService};
 
 pub fn configure(config: &mut web::ServiceConfig, app_data: web::Data<AppState>) {
-        
-        let country_repo = CountryRepoService::new(app_data.db_pool.clone());
-        let country_service = web::Data::new(CountryService::new(country_repo));
+    let country_repo = CountryRepoService::new(app_data.db_pool.clone());
+    let country_service = web::Data::new(CountryService::new(country_repo));
 
-        config.app_data(country_service);
+    config.app_data(country_service);
 }
