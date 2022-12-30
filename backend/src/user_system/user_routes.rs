7d9db0db9c82887fad776934d5c36e8d28503375
Jibao Mansaray <jibaomansaray@gmail.com>
2022-12-30 14:11:50 +0000

    User routes: Register `user update` route

--- backend/src/user_system/user_routes.rs
+++ backend/src/user_system/user_routes.rs
@@ -6,6 +6,7 @@ pub fn register_api_routes(scope: Scope) -> Scope {
     scope.service(user_controllers::api_controllers::user_my_information_controller::handler)
         .service(user_controllers::api_controllers::user_vapid_key_controller::handler)
         .service(user_controllers::api_controllers::user_delete_my_account_controller::handler)
+        .service(user_controllers::api_controllers::user_update_controller::handler)
 }
 
 pub fn register_public_routes(scope: Scope) -> Scope {
