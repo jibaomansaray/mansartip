12c87f2a97aec365182a8c0e2c318ba229ab606d
Jibao Mansaray <jibaomansaray@gmail.com>
2022-12-30 09:35:29 +0000

    User Controllers: Implement `delete my account` controller

--- backend/src/user_system/user_controllers/api_controllers/user_delete_my_account_controller.rs
+++ backend/src/user_system/user_controllers/api_controllers/user_delete_my_account_controller.rs
@@ -1,7 +1,25 @@
+use actix_web::{delete, web, Responder, Result};
 
-use actix_web::{delete, web, HttpRequest , Responder, Result};
+use crate::{
+    app::app_helpers::authenticated_user::AuthenticatedUser,
+    user_system::{
+        user_dtos::user_entity_api_response_dto::UserEntityApiResponseDto,
+        user_services::user_service::UserService,
+    },
+};
 
 #[delete("user/my-account")]
-pub(crate) async fn handler() -> impl Responder {
-    HttpResponse::Ok().body("user delete my account")
+pub(crate) async fn handler(
+    auth_user: AuthenticatedUser,
+    user_service: web::Data<UserService>,
+) -> Result<impl Responder> {
+    let user = auth_user.into_inner();
+    let result = user_service.delete_my_account(&user.internal_id).await;
+    match result {
+        Ok(user) => Ok(web::Json(UserEntityApiResponseDto::new(user))),
+        Err(e) => Ok(web::Json(UserEntityApiResponseDto::new_not_found(
+            Some(e.message),
+            Some(e.code),
+        ))),
+    }
 }
