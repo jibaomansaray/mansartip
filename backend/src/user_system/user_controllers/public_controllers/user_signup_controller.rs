7901048187175be7c5f1755318cbd99669ec82fc
Jibao Mansaray <jibaomansaray@gmail.com>
2022-12-30 06:27:15 +0000

    User Signup Controller: Handle the `result` returned form the service

--- backend/src/user_system/user_controllers/public_controllers/user_signup_controller.rs
+++ backend/src/user_system/user_controllers/public_controllers/user_signup_controller.rs
@@ -1,4 +1,10 @@
-use crate::user_system::{user_entities::UserEntity, user_services::user_service::UserService};
+use crate::{
+    app::app_state::AppState,
+    user_system::{
+        user_dtos::user_entity_api_response_dto::UserEntityApiResponseDto,
+        user_entities::UserEntity, user_services::user_service::UserService,
+    },
+};
 use actix_web::{post, web, Responder, Result};
 use serde::{Deserialize, Serialize};
 
@@ -12,12 +18,21 @@ pub(crate) struct SignupPayload {
 #[post("user/signup")]
 pub(crate) async fn handler(
     payload: web::Json<SignupPayload>,
-    _user_service: web::Data<UserService>,
+    user_service: web::Data<UserService>,
+    app_data: web::Data<AppState>,
 ) -> Result<impl Responder> {
-    let mut user_entity = UserEntity::default();
-    user_entity.username = payload.username.clone();
-    user_entity.email = payload.email.clone();
-    user_entity.password = payload.password.clone();
+    let result = user_service
+        .create_user(&payload.username, &payload.email, &payload.password)
+        .await;
 
-    Ok(web::Json(user_entity))
+    match result {
+        Ok(user_entity) => Ok(web::Json(UserEntityApiResponseDto::new_auth_entity(
+            user_entity,
+            Some(app_data.vapid_public_key.clone()),
+        ))),
+        Err(e) => Ok(web::Json(UserEntityApiResponseDto::new_not_found(
+            Some(e.message),
+            Some(e.code),
+        ))),
+    }
 }
