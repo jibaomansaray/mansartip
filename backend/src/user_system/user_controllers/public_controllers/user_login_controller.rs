d90504c75e2dac05432008f40197e541dd10b732
Jibao Mansaray <jibaomansaray@gmail.com>
2022-12-30 02:25:11 +0000

    User System: Set auth cookie
    Also added the app's push notification public key
    to the response.

--- backend/src/user_system/user_controllers/public_controllers/user_login_controller.rs
+++ backend/src/user_system/user_controllers/public_controllers/user_login_controller.rs
@@ -1,24 +1,45 @@
-use actix_web::{post, web, Responder, Result };
+use actix_web::{cookie::Cookie, post, web, HttpResponse, HttpResponseBuilder, Responder, Result};
+use cookie::{CookieJar, Key};
 use serde::{Deserialize, Serialize};
 
-use crate::user_system::{user_services::user_service::UserService, user_dtos::user_entity_api_response_dto::UserEntityApiResponseDto};
-
+use crate::{user_system::{
+    user_dtos::user_entity_api_response_dto::UserEntityApiResponseDto, user_entities::UserEntity,
+    user_services::user_service::UserService,
+}, app::app_state::AppState};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub(crate) struct LoginPayload {
-  pub username: String,
-  pub password: String
+    pub username: String,
+    pub password: String,
 }
 
 #[post("user/login")]
-pub(crate) async fn handler(payload: web::Json<LoginPayload>, user_service: web::Data<UserService>) -> Result<impl Responder> {
-  let user = user_service.log_user_in(&payload.username, &payload.password).await;
-  
-  if let Some(auth_user) = user { 
-    // @todo send token cookie
-    // @todo auth user response must inclue the push notication token
-    Ok(web::Json(UserEntityApiResponseDto::new(auth_user)))
-  } else {
-    Ok(web::Json(UserEntityApiResponseDto::new_not_found(Some(String::from("posted data is incorrect")), Some(String::from("posted_data_incorrect")))))
-  }
+pub(crate) async fn handler(
+    payload: web::Json<LoginPayload>,
+    user_service: web::Data<UserService>,
+    app_data: web::Data<AppState>
+) -> Result<impl Responder> {
+    let user = user_service
+        .log_user_in(&payload.username, &payload.password)
+        .await;
+
+    if let Some(auth_user) = user {
+        // @todo secure cookie
+        let cookie = Cookie::build("_t", &auth_user.token).permanent().secure(true).path("/").finish();
+
+        let response = HttpResponse::Ok()
+            .cookie(cookie)
+            .json(web::Json(UserEntityApiResponseDto::new_auth_entity(auth_user, Some(app_data.vapid_public_key.clone()))));
+
+        Ok(response)
+    } else {
+        let response = HttpResponse::Ok().json(web::Json(
+            UserEntityApiResponseDto::<UserEntity>::new_not_found(
+                Some(String::from("posted data is incorrect")),
+                Some(String::from("posted_data_incorrect")),
+            ),
+        ));
+
+        Ok(response)
+    }
 }
