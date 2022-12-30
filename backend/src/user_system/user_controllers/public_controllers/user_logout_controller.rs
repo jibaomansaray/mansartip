0844775f25a760a1101d37074b6bec8cc977889b
Jibao Mansaray <jibaomansaray@gmail.com>
2022-12-30 06:30:26 +0000

    User Login Controller: The cookie is now being cleared
    When the user logged out, the `auth` cookie is now being'
    cleared.

--- backend/src/user_system/user_controllers/public_controllers/user_logout_controller.rs
+++ backend/src/user_system/user_controllers/public_controllers/user_logout_controller.rs
@@ -1,4 +1,5 @@
-use actix_web::{get, web, Responder, Result};
+use actix_web::{get, web, Responder, Result, HttpResponse};
+use cookie::Cookie;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -6,11 +7,17 @@ pub(crate) struct LogoutRespnse {
     success: bool,
 }
 
-
 #[get("user/logout")]
 pub(crate) async fn handler() -> Result<impl Responder> {
-    // @todo clear cookie
-    Ok(web::Json(LogoutRespnse{
-      success: true
-    }))
+    let cookie = Cookie::build("_t", "")
+        .permanent()
+        .secure(true)
+        .path("/")
+        .finish();
+
+    let response = HttpResponse::Ok()
+        .cookie(cookie)
+        .json(web::Json(LogoutRespnse { success: true }));
+
+    Ok(response)
 }
