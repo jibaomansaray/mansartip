42add6ded489d3e9cc17af828a6957e0c100a931
Jibao Mansaray <jibaomansaray@gmail.com>
2022-12-30 09:37:54 +0000

    User API Controllers: Register `delete my account` controller

--- backend/src/user_system/user_routes.rs
+++ backend/src/user_system/user_routes.rs
@@ -5,6 +5,7 @@ use crate::user_system::user_controllers;
 pub fn register_api_routes(scope: Scope) -> Scope {
     scope.service(user_controllers::api_controllers::user_my_information_controller::handler)
         .service(user_controllers::api_controllers::user_vapid_key_controller::handler)
+        .service(user_controllers::api_controllers::user_delete_my_account_controller::handler)
 }
 
 pub fn register_public_routes(scope: Scope) -> Scope {
