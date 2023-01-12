d271713a2ef65aba1305b49e5ab8949e4b12bdaf
Jibao Mansaray <jibaomansaray@gmail.com>
2023-01-12 11:33:04 +0000

    Refactore game and country's controller name

--- backend/src/api_routes.rs
+++ backend/src/api_routes.rs
@@ -2,16 +2,16 @@ use actix_web::web;
 
 pub(crate) mod auth_middleware;
 
-use crate::country::country_routes;
-use crate::game::game_routes;
+use crate::country_system::country_routes;
+use crate::match_system::match_routes;
 use crate::user_system::user_routes;
 
 pub fn configure(config: &mut web::ServiceConfig) {
     let mut api_routes = web::scope("/api/v1");
 
     api_routes = country_routes::register_api_routes(api_routes);
-    api_routes = game_routes::register_api_routes(api_routes);
     api_routes = user_routes::register_api_routes(api_routes);
+    api_routes = match_routes::register_api_routes(api_routes);
 
     // api routes middleware
     let api_service = api_routes.wrap(auth_middleware::Auth {});
