87a392a12f4b75e586d180b7e216442eca9b6d32
Jibao Mansaray <jibaomansaray@gmail.com>
2023-01-13 17:16:23 +0000

    User System: Refactor user entity
    Add the ability to pluck entity values from db rows.
    Add other methods that makes it easy to use references.

--- backend/src/user_system/user_entities/user_entity.rs
+++ backend/src/user_system/user_entities/user_entity.rs
@@ -1,8 +1,15 @@
-use crate::app::{
-    app_helpers::database_datetime_helper::{
-        created_at_field_value, deleted_at_field_value, updated_at_field_value,
+use std::collections::HashMap;
+
+use crate::{
+    app::{
+        app_helpers::database_datetime_helper::{
+            created_at_field_value, datetime_from_db_row_field, deleted_at_field_value,
+            updated_at_field_value,
+        },
+        app_state::DbRow,
     },
-    app_state::DbRow,
+    country_system::country_dtos::country_entity_api_dto::CountryEntityApiDto,
+    user_system::user_dtos::user_entity_api_dto::UserEntityDto,
 };
 use chrono::prelude::*;
 use rand::Rng;
@@ -83,36 +90,15 @@ pub struct UserEntity {
 
 impl From<DbRow> for UserEntity {
     fn from(row: DbRow) -> Self {
-        let username: String = row.try_get("username").unwrap_or_default();
-        Self {
-            id: row.try_get("id").unwrap_or_default(),
-            internal_id: row.try_get("internalId").unwrap_or_default(),
-            role: match row
-                .try_get("role")
-                .unwrap_or_else(|_| String::from("user"))
-                .as_str()
-            {
-                "admin" => UserRole::Admin,
-                _ => UserRole::User,
-            },
-            user_type: match row
-                .try_get("type")
-                .unwrap_or_else(|_| String::from("user"))
-                .as_str()
-            {
-                "human" => UserType::Human,
-                _ => UserType::Bot,
-            },
-            username: row.try_get("username").unwrap_or_default(),
-            email: row.try_get("email").unwrap_or_default(),
-            password: row.try_get("password").unwrap_or_default(),
-            token: row.try_get("token").unwrap_or_default(),
-            data: row.try_get("data").unwrap_or_default(),
-            avater: format!("/static/user/{}.png", username),
-            created_at: created_at_field_value(&row),
-            updated_at: updated_at_field_value(&row),
-            deleted_at: deleted_at_field_value(&row),
-        }
+        let map = HashMap::new();
+        Self::pluck_values(&row, &map).unwrap()
+    }
+}
+
+impl From<&DbRow> for UserEntity {
+    fn from(row: &DbRow) -> Self {
+        let map = HashMap::new();
+        Self::pluck_values(row, &map).unwrap()
     }
 }
 
@@ -136,6 +122,17 @@ impl Default for UserEntity {
     }
 }
 
+pub struct UserJoinProcessor<'a> {
+    pub fields: String,
+    pub field_map: HashMap<&'a str, String>,
+}
+
+impl<'a> UserJoinProcessor<'a> {
+    pub fn transform(&self, row: &DbRow) -> Option<UserEntityDto> {
+        UserEntity::new_for_join(row, &self.field_map)
+    }
+}
+
 impl UserEntity {
     pub fn reset_token(&mut self) {
         self.token = Self::generate_token()
@@ -150,5 +147,141 @@ impl UserEntity {
         }
 
         hex
+    }
+
+    pub fn new_for_join(row: &DbRow, map: &HashMap<&str, String>) -> Option<UserEntityDto> {
+        if let Some(entity) = Self::pluck_values(row, map) {
+            Some(UserEntityDto::new(entity))
+        } else {
+            None
+        }
+    }
+
+    fn pluck_values(row: &DbRow, map: &HashMap<&str, String>) -> Option<Self> {
+        let id_field = match map.get("id") {
+            Some(e) => e.split("as").last().unwrap().trim(),
+            _ => "id",
+        };
+        let internal_id_field = match map.get("internalId") {
+            Some(e) => e.split("as").last().unwrap().trim(),
+            _ => "internalId",
+        };
+
+        let role_field = match map.get("role") {
+            Some(e) => e.split("as").last().unwrap().trim(),
+            _ => "role",
+        };
+        let type_field = match map.get("type") {
+            Some(e) => e.split("as").last().unwrap().trim(),
+            _ => "type",
+        };
+
+        let username_field = match map.get("username") {
+            Some(e) => e.split("as").last().unwrap().trim(),
+            _ => "username",
+        };
+
+        let data_field = match map.get("data") {
+            Some(e) => e.split("as").last().unwrap().trim(),
+            _ => "data",
+        };
+
+        let email_field = match map.get("email") {
+            Some(e) => e.split("as").last().unwrap().trim(),
+            _ => "email",
+        };
+        let password_field = match map.get("password") {
+            Some(e) => e.split("as").last().unwrap().trim(),
+            _ => "password",
+        };
+
+        let token_field = match map.get("token") {
+            Some(e) => e.split("as").last().unwrap().trim(),
+            _ => "token",
+        };
+
+        let created_at_field = match map.get("createdAt") {
+            Some(e) => e.split("as").last().unwrap().trim(),
+            _ => "careatedAt",
+        };
+
+        let updated_at_field = match map.get("updatedAt") {
+            Some(e) => e.split("as").last().unwrap().trim(),
+            _ => "updatedAt",
+        };
+        let deleted_at_field = match map.get("deletedAt") {
+            Some(e) => e.split("as").last().unwrap().trim(),
+            _ => "deletedAt",
+        };
+
+        let id: u64 = row.try_get(id_field).unwrap_or_default();
+        let username: String = row.try_get(username_field).unwrap_or_default();
+
+        if id > 0 {
+            Some(Self {
+                id,
+                internal_id: row.try_get(internal_id_field).unwrap_or_default(),
+                role: match row
+                    .try_get(role_field)
+                    .unwrap_or_else(|_| String::from("user"))
+                    .as_str()
+                {
+                    "admin" => UserRole::Admin,
+                    _ => UserRole::User,
+                },
+                user_type: match row
+                    .try_get(type_field)
+                    .unwrap_or_else(|_| String::from("user"))
+                    .as_str()
+                {
+                    "human" => UserType::Human,
+                    _ => UserType::Bot,
+                },
+                username: username.clone(),
+                email: row.try_get(email_field).unwrap_or_default(),
+                password: row.try_get(password_field).unwrap_or_default(),
+                token: row.try_get(token_field).unwrap_or_default(),
+                data: row.try_get(data_field).unwrap_or_default(),
+                avater: format!("/static/user/{}.png", username),
+                created_at: datetime_from_db_row_field(created_at_field, &row),
+                updated_at: datetime_from_db_row_field(updated_at_field, &row),
+                deleted_at: datetime_from_db_row_field(deleted_at_field, &row),
+            })
+        } else {
+            None
+        }
+    }
+
+    pub fn generate_join_fields(prefix: Option<&str>) -> UserJoinProcessor {
+        let table = prefix.unwrap_or_else(|| "user");
+        let as_field = |name| format!("`{0}`.`{1}` as {0}_{1}", table, name);
+
+        let map = HashMap::from([
+            ("id", as_field("id")),
+            ("internalId", as_field("internalId")),
+            ("role", as_field("role")),
+            ("type", as_field("type")),
+            ("username", as_field("username")),
+            ("email", as_field("email")),
+            ("password", as_field("password")),
+            ("token", as_field("token")),
+            ("createdAt", as_field("createdAt")),
+            ("updatedAt", as_field("updatedAt")),
+            ("deletedAt", as_field("deletedAt")),
+        ]);
+
+        let mut fields = String::new();
+
+        for entry in &map {
+            if !fields.is_empty() {
+                fields.push_str(",")
+            }
+            fields.push_str(entry.1.as_str())
+        }
+
+        UserJoinProcessor {
+            fields,
+            field_map: map,
+        }
     }
 }
