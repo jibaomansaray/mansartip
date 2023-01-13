d3eac914bee6f3c9da7320948eca46c3b60646be
Jibao Mansaray <jibaomansaray@gmail.com>
2023-01-13 04:11:42 +0000

    Match System: Return an API Response DTO

--- backend/src/match_system/match_controllers/api_controllers/match_today_matches_controller.rs
+++ backend/src/match_system/match_controllers/api_controllers/match_today_matches_controller.rs
@@ -1,16 +1,21 @@
 use actix_web::{get, web, Responder, Result};
 
-use crate::{app::app_state::AppState, match_system::match_services::match_service::MatchService};
+use crate::{
+    app::{app_dtos::api_response_dto::ApiResponseDto, app_state::AppState},
+    match_system::{
+        match_dtos::match_entity_api_dto::MatchEntityDto,
+        match_services::match_service::MatchService,
+    },
+};
 
 #[get("match/todays")]
 pub(crate) async fn handler(
     match_service: web::Data<MatchService>,
     app_data: web::Data<AppState>,
 ) -> Result<impl Responder> {
-    Ok(web::Json(
-        match_service
-            .get_today_matches(Some(app_data.year))
-            .await
-            .unwrap(),
-    ))
+    let response = ApiResponseDto::new(MatchEntityDto::from_entities(
+        match_service.get_today_matches(app_data.year).await,
+    ));
+
+    Ok(web::Json(response))
 }
