87a26c442e474d6d944254d69ba952eddcd5109c
Jibao Mansaray <jibaomansaray@gmail.com>
2023-01-13 17:27:57 +0000

    Country System: Use the generic `ApiReponse`

--- backend/src/country_system/country_controllers/get_all_countries_controller.rs
+++ backend/src/country_system/country_controllers/get_all_countries_controller.rs
@@ -1,8 +1,11 @@
 use actix_web::{get, web, HttpRequest, Responder, Result};
 
-use crate::country_system::{
-    country_dtos::countries_api_response_dto::CountriesApiResponseDto,
-    country_services::country_service::CountryService,
+use crate::{
+    app::app_dtos::api_response_dto::ApiResponseDto,
+    country_system::{
+        country_dtos::country_entity_api_dto::CountryEntityApiDto,
+        country_services::country_service::CountryService,
+    },
 };
 
 #[get("country/all")]
@@ -10,7 +13,9 @@ pub(crate) async fn handler(
     _req: HttpRequest,
     country_repo: web::Data<CountryService>,
 ) -> Result<impl Responder> {
-    let country_entities = country_repo.as_ref().get_all_countries().await;
+    let response = ApiResponseDto::new(CountryEntityApiDto::from_entities(
+        country_repo.as_ref().get_all_countries().await,
+    ));
 
-    Ok(web::Json(CountriesApiResponseDto::new(country_entities)))
+    Ok(web::Json(response))
 }
--- backend/src/country_system/country_controllers/get_country_by_id_controller.rs
+++ backend/src/country_system/country_controllers/get_country_by_id_controller.rs
@@ -1,19 +1,23 @@
-use crate::country_system::{
-    country_dtos::country_api_response_dto::CountryApiResponseDto,
-    country_services::country_service::CountryService,
+use crate::{
+    app::app_dtos::api_response_dto::ApiResponseDto,
+    country_system::{
+        country_dtos::country_entity_api_dto::CountryEntityApiDto,
+        country_services::country_service::CountryService,
+    },
 };
 use actix_web::{get, web, Responder, Result};
 
 #[get("country/{country_id}")]
 pub(crate) async fn handler(
-    path: web::Path<i32>,
+    path: web::Path<String>,
     country_service: web::Data<CountryService>,
 ) -> Result<impl Responder> {
     let id = path.into_inner();
-    let mut response = CountryApiResponseDto::new_not_found();
+    let mut response =
+        ApiResponseDto::new_not_found(Some("record not found"), Some("record_not_found"));
 
-    if let Some(country) = country_service.get_country_by_id(id).await {
-        response = CountryApiResponseDto::new(country);
+    if let Some(country) = country_service.get_country_by_internal_id(&id).await {
+        response = ApiResponseDto::new(CountryEntityApiDto::from(country));
     }
 
     Ok(web::Json(response))
