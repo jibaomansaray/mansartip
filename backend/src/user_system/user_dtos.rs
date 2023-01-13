88fc114e574bfa2040136d5b8789385366876278
Jibao Mansaray <jibaomansaray@gmail.com>
2023-01-13 17:14:49 +0000

    User System: Expose the `user entity` DTO

--- backend/src/user_system/user_dtos.rs
+++ backend/src/user_system/user_dtos.rs
@@ -1,3 +1,3 @@
-pub(crate) mod push_subscription_api_response_dto;
-pub(crate) mod user_entity_api_response_dto;
-pub(crate) mod user_entity_update_api_dto;
+pub mod user_auth_entity_dto;
+pub mod user_entity_api_dto;
+pub mod user_entity_update_api_dto;
