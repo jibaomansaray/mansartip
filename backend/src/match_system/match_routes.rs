e759f8d716e9346ae9a767aa58388da937b03a2c
Jibao Mansaray <jibaomansaray@gmail.com>
2023-01-13 03:59:22 +0000

    Match System: Register `all` API route

--- backend/src/match_system/match_routes.rs
+++ backend/src/match_system/match_routes.rs
@@ -3,7 +3,9 @@ use actix_web::Scope;
 use super::match_controllers;
 
 pub fn register_api_routes(scope: Scope) -> Scope {
-    scope.service(match_controllers::api_controllers::match_today_matches_controller::handler)
+    scope
+        .service(match_controllers::api_controllers::match_today_matches_controller::handler)
+        .service(match_controllers::api_controllers::match_all_matches_controller::handler)
 }
 
 pub fn register_public_routes(scope: Scope) -> Scope {
