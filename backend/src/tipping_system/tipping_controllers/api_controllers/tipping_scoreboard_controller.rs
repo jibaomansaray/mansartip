bde9e8316b47eeedbb998fe75aabd16563ca4dda
Jibao Mansaray <jibaomansaray@gmail.com>
2023-01-14 11:44:39 +0000

    Tipping System: Remove unused namespaces

--- backend/src/tipping_system/tipping_controllers/api_controllers/tipping_scoreboard_controller.rs
+++ backend/src/tipping_system/tipping_controllers/api_controllers/tipping_scoreboard_controller.rs
@@ -1,15 +1,16 @@
 use actix_web::{get, web, Responder, Result};
 
 use crate::{
-    app::app_dtos::api_response_dto::ApiResponseDto,
+    app::{app_dtos::api_response_dto::ApiResponseDto, app_state::AppState},
     tipping_system::tipping_services::tipping_scoreboard_service::TippingScoreboardService,
 };
 
 #[get("tip/scoreboard")]
 pub async fn handler(
     scoreboard_service: web::Data<TippingScoreboardService>,
+    app_state: web::Data<AppState>,
 ) -> Result<impl Responder> {
-    let response = scoreboard_service.entries(2022, 25).await;
+    let response = scoreboard_service.entries(app_state.year, 25).await;
 
     Ok(web::Json(ApiResponseDto::new(response)))
 }
