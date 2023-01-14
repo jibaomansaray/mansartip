60693528b16f8b749dd03880665776bbfb0ed9e2
Jibao Mansaray <jibaomansaray@gmail.com>
2023-01-14 11:45:36 +0000

    Tipping System: Register `my score` controller

--- backend/src/tipping_system/tipping_routes.rs
+++ backend/src/tipping_system/tipping_routes.rs
@@ -3,7 +3,9 @@ use actix_web::Scope;
 use super::tipping_controllers;
 
 pub fn register_api_routes(scope: Scope) -> Scope {
-    scope.service(tipping_controllers::api_controllers::tipping_scoreboard_controller::handler)
+    scope
+        .service(tipping_controllers::api_controllers::tipping_scoreboard_controller::handler)
+        .service(tipping_controllers::api_controllers::tipping_my_score_controller::handler)
 }
 
 pub fn register_public_routes(scope: Scope) -> Scope {
